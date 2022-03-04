8fe33b3cdce4dce49523682a60da096f66e37c25
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-03-04 06:38:27 -0300

    js

--- EXTENSORGOOGLE/index.js
+++ EXTENSORGOOGLE/index.js
@@ -1,11 +1,18 @@
-let myleads = []
+let myleads = ['www.awesome.com', 'www.epiclead.com', 'www.greatle.com']
 const inputbtn = window.document.getElementById('input-btn')
 const inputEl = window.document.getElementById('input-el')
 
+const ul = window.document.getElementById('ul-el')
+
 /* COLOCOU A FUNCTION DENTRO DESSA PARADA ABAIXO.
-PEGOU O LET INPUTBTN CRIOU UM ADDEVENTLISTENER CLICK COM 
+PEGOU O LET INPUT CRIOU UM ADDEVENTLISTENER CLICK COM 
 A FUNCTION() */
 inputbtn.addEventListener('click', function(){
-    console.log('clicar no botão pelo addEventListener')
+   myleads.push(inputEl.value)
+   console.log(myleads) 
 })
 
+for(let i = 0; i < myleads.length; i++){
+     
+    ul.innerHTML += "<li>" + myleads[i] + "</li>"
+}
