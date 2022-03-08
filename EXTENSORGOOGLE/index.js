bf1157a48d23f7a0301c047af611d3f47eee8aa0
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-03-08 13:07:43 -0300

    atu

--- EXTENSORGOOGLE/index.js
+++ EXTENSORGOOGLE/index.js
@@ -1,18 +1,22 @@
-let myleads = ['www.awesome.com', 'www.epiclead.com', 'www.greatle.com']
+let myleads = []
 const inputbtn = window.document.getElementById('input-btn')
 const inputEl = window.document.getElementById('input-el')
-
-const ul = window.document.getElementById('ul-el')
+const ulEl = window.document.getElementById('ul-le')
 
 /* COLOCOU A FUNCTION DENTRO DESSA PARADA ABAIXO.
 PEGOU O LET INPUT CRIOU UM ADDEVENTLISTENER CLICK COM 
 A FUNCTION() */
-inputbtn.addEventListener('click', function(){
-   myleads.push(inputEl.value)
-   console.log(myleads) 
-})
+inputbtn.addEventListener('click', function() {
+    myleads.push(inputEl.value)  
+    inputEl.value = ''
+    renderLeads() 
+}) 
 
-for(let i = 0; i < myleads.length; i++){
-     
-    ul.innerHTML += "<li>" + myleads[i] + "</li>"
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myleads.length; i++) {
+        listItems += "<li> <a href='' target='_blank'>" + myleads[i] + " </a> </li>"
+    } 
+    ulEl.innerHTML = listItems
 }
