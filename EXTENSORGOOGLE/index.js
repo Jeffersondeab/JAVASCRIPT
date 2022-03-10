b1618f735292f28140942ad9847f5de1215f9cb7
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-03-10 07:36:08 -0300

    js

--- EXTENSORGOOGLE/index.js
+++ EXTENSORGOOGLE/index.js
@@ -1,16 +1,44 @@
 let myleads = []
+let oldleads = []
 const inputbtn = window.document.getElementById('input-btn')
 const inputEl = window.document.getElementById('input-el')
 const ulEl = window.document.getElementById('ul-le')
 
+const deleteu = window.document.getElementById('delete')
+ 
+/* SISTEMA PARA SALVAR DADOS
+QUANDO ATUALIZAR A PÁGINA  */
+ 
+let myLeadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+
+if (myLeadsFromLocalStorage){
+    myleads = myLeadsFromLocalStorage
+    renderLeads()
+}
+
+
+
+deleteu.addEventListener('click', function(){
+    localStorage.clear()
+    myleads = []  
+    renderLeads()
+})
+
+
+
 /* COLOCOU A FUNCTION DENTRO DESSA PARADA ABAIXO.
 PEGOU O LET INPUT CRIOU UM ADDEVENTLISTENER CLICK COM 
 A FUNCTION() */
+
+
 inputbtn.addEventListener('click', function() {
     myleads.push(inputEl.value)  
     inputEl.value = ''
+    localStorage.setItem('myLeads',JSON.stringify(myleads))
     renderLeads() 
 }) 
+
+ 
 
 
 function renderLeads() {
